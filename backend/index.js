342696c37d6bccf3580334694211b9208c51acf1
Arunkumar <arunkumar.s@indiumsoft.com>
2023-10-05 18:59:20 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require("cors")
+const mongoose = require('mongoose')
+const userRoutes = require("./routes/userRoutes")
+require("dotenv").config()
+
+const app = express()
+
+
+PORT = process.env.PORT | 5000
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/auth", userRoutes)
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("db connected successfully")
+}).catch((err) => {
+  console.log(err.message)
+})
+
+
+const server = app.listen(PORT, () => {
+  console.log(`Server stated on port ${PORT}`)
+})

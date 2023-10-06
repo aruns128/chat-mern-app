e90339ddd57f2468a66a8d9edb3a7a92ab8e8d6d
Arunkumar <arunkumar.s@indiumsoft.com>
2023-10-06 14:38:42 +0530

    set avatar component completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ import { Register } from './pages/Register';
 import { Login } from './pages/Login';
 import { Chat } from './pages/Chat';
 import 'react-toastify/dist/ReactToastify.css';
+import { SetAvatar } from './components/SetAvatar';
+
+
 
 function App() {
   return (
@@ -12,6 +15,7 @@ function App() {
       <Routes>
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/setAvatar' element={<SetAvatar />} />
         <Route path='/' element={<Chat />} />
       </Routes>
     </BrowserRouter>
